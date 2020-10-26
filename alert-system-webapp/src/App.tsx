9092ba43844bb3ed58d3502992ac020da5f77eb1
Jørgen H <34099284+Schweizeren@users.noreply.github.com>
2020-10-26 12:13:49 +0100

    Data presentation
    Data is presented in the easiest way and moq data is used.

--- alert-system-webapp/src/App.tsx
+++ alert-system-webapp/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
-import {Home} from 'pages/home/home';
+import Home from './pages/home/home';
 
 const App = () => {
   return (
--- alert-system-webapp/src/data/AlarmService.ts
+++ alert-system-webapp/src/data/AlarmService.ts
@@ -1,15 +1,21 @@
-import { Guid } from 'guid-typescript';
-import Alarm from "../models/Alarm";
+import {Alarm} from '../models/Alarm';
+import {Guid} from "guid-typescript";
 
 export const GetAlarms = () : Alarm[] => {
     return alarmList;
+};
+
+export const GetFormattedDate = (dateToFormat: Date) : string => {
+    const something : string = `${dateToFormat.getDate()}/${dateToFormat.getMonth()}/${dateToFormat.getFullYear()} ${dateToFormat.getHours()}:${dateToFormat.getMinutes()}`;
+    return something;
+
 };
 
 const alarmList: Alarm[] = [
     {
         id : Guid.create(),
         alarmId : Guid.create(),
-        date : Date.now()
+        date : new Date(400000000000)
     },
     {
         id : Guid.create(),
@@ -19,6 +25,6 @@ const alarmList: Alarm[] = [
     {
         id : Guid.create(),
         alarmId : Guid.create(),
-        date : Date.now()
+        date : new Date(480000000000)
     }
 ];
