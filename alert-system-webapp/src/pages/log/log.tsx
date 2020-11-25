afd1eb3225d6800edda9e931f6f471fd81f07cdd
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-11-25 15:12:31 +0100

    Changed css and added more stuff to log page

--- alert-system-webapp/src/pages/log/log.tsx
+++ alert-system-webapp/src/pages/log/log.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {GetAlarms, GetFormattedDate} from "../../data/AlarmService";
 import {AlarmLog} from "../../models/AlarmLog";
-import {AlarmLogs} from "../alarmLogProps/alarmLogProps";
 import './log.css';
 import { useHistory } from "react-router-dom";
 
@@ -29,39 +28,52 @@ const Log: React.FC = () => {
 
     return (
         <div className="log__page">
-            <h1>Alarm Log</h1>
-            <button className={"log__home-button"} onClick={handleClick}>
-                Home
-            </button>
-            <table className="log__table">
-                <thead>
-                    <tr>
-                        <th>Machine ID</th>
-                        <th>Alarm Code</th>
-                        <th>Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {alarms.map((data, index) => {
-                        return (
-                            <tr key={`data-${index}`} onClick={() => openPopUp(data)}>
-                                <td>
-                                    {data.machine.machineId}
-                                </td>
-                                <td>
-                                    {data.alarm.code}
-                                </td>
-                                <td>
-                                    {GetFormattedDate(data.date)}
-                                </td>
-                            </tr>
-                        )
-                    })}
+            <div className="log__header">
+
+            </div>
+            <div className="log__table-container">
+                <table className="log__table">
+                    <thead>
+                        <tr>
+                            <th>Machine ID</th>
+                            <th>Alarm Code</th>
+                            <th>Date</th>
+                            <th>Last status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {alarms.map((data, index) => {
+                            return (
+                                <tr key={`data-${index}`} onClick={() => openPopUp(data)}>
+                                    <td>
+                                        {data.machine.machineId}
+                                    </td>
+                                    <td>
+                                        {data.alarm.code}
+                                    </td>
+                                    <td>
+                                        {GetFormattedDate(data.date)}
+                                    </td>
+                                    <td>
+                                        Fixed
+                                    </td>
+                                </tr>
+                            )
+                        })}
                 </tbody>
             </table>
-            {open && alarm && (
-                <AlarmLogs open={open} handleClose={() => setOpen(false)} alarm={alarm}/>
-            )}
+            </div>
+            <div className="log__details">
+                {alarm && (
+                    <div>
+                        <h1>Details</h1>
+                        <h2>Alarm code: {alarm.alarm.code}</h2>
+                        <h2>Alarm description: {alarm.alarm.description}</h2>
+                        <h2>Occured: {GetFormattedDate(alarm.date)}</h2>
+                        <h2>From machine: {alarm.machine.machineId}</h2>
+                    </div>
+                )}
+            </div>
         </div>
 )};
 
