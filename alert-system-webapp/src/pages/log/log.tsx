56ba702821e4bbf977fa98138d66328e3de4189c
Frederik Jensen <kungfu15447@users.noreply.github.com>
2020-12-02 13:59:58 +0100

    Can now filter with date time and how much time in between

--- alert-system-webapp/src/pages/log/log.tsx
+++ alert-system-webapp/src/pages/log/log.tsx
@@ -9,15 +9,68 @@ const Log: React.FC = () => {
     const [alarms, setAlarms] = useState<AlarmLog[]>([]);
     const [alarm, setAlarm] = useState<AlarmLog>();
     const [loading, setLoading] = useState(false);
+    const [filterList, setFilterList] = useState<AlarmLog[]>([]);
+    const [filterDate, setFilterDate] = useState<Date>();
+    const [filterHour, setFilterHour] = useState(0);
+    const [filterTime, setFilterTime] = useState(0);
+    const [filter, setFilter] = useState(false);
 
     useEffect(() => {
         setLoading(true);
         GetAlarms().then(list => {
             setAlarms(list);
+            setFilterList(list);
         }).finally(() => {
             setLoading(false);
         });
     }, []);
+
+    const filterAlarmLogs = () => {
+        let filteredList = [];
+
+        if (filterDate) {
+            filteredList = alarms.filter(data => checkIfDateIsEqual(data.date));
+
+            filteredList = filteredList.filter(data => checkIfDateIsInTimeFrame(data.date));
+            setFilterList(filteredList);
+        }
+    }
+
+    const checkIfDateIsEqual = (date: Date) => {
+        return date.getDate() === filterDate?.getDate() && 
+            date.getMonth() === filterDate.getMonth() && 
+            date.getFullYear() === filterDate.getFullYear();
+    }
+
+    const checkIfDateIsInTimeFrame = (date: Date) => {
+        if (filterDate) {
+            const timeFrame = filterDate;
+            timeFrame.setHours(filterHour);
+
+            const dateTime = date.getTime();
+            const timeFrameTime = timeFrame.getTime();
+
+
+            return dateTime >= timeFrameTime && dateTime <= timeFrameTime + filterTime; 
+        }
+        return true;
+    }
+
+    const createHourOptions = () => {
+        const numbers: number[] = [];
+        for (let i = 0; i < 24; i++) {
+            numbers.push(i);
+        }
+        return numbers;
+    }
+
+    useEffect(() => {
+        if (filter) {
+            filterAlarmLogs();
+        } else {
+            setFilterList(alarms);
+        }
+    }, [filterDate, filter, filterHour, filterTime])
 
     return (
         <div className="log__page">
@@ -26,19 +79,29 @@ const Log: React.FC = () => {
             </div>
             <div className="log__table-container">
                 <div className="log__table-filter-container">
-                    <input type="date"/>
-                    <select>
-                        <option>13:00</option>
-                        <option>14:00</option>
-                        <option>15:00</option>
+                    <input type="date" onChange={date => setFilterDate(new Date(date.target.value))}/>
+                    <select onChange={hour => setFilterHour(parseInt(hour.target.value))}>
+                        {createHourOptions().map((data) => {
+                            let optionText = "";
+                            if (data < 10) {
+                                optionText = `0${data}:00`;
+                            } else {
+                                optionText = `${data}:00`;
+                            }
+
+                            return (
+                                <option value={data} key={data}>{optionText}</option>
+                            )
+                        })}
                     </select>
-                    <select>
-                        <option>15 min</option>
-                        <option>30 min</option>
-                        <option>45 min</option>
-                        <option>1 time</option>
-                        <option>2 timer</option>
-                        <option>3 timer</option>
+                    <select onChange={data => setFilterTime(parseInt(data.target.value))}>
+                        <option value={0}>0 min</option>
+                        <option value={900000}>15 min</option>
+                        <option value={1800000}>30 min</option>
+                        <option value={2700000}>45 min</option>
+                        <option value={3600000}>1 time</option>
+                        <option value={7200000}>2 timer</option>
+                        <option value={10800000}>3 timer</option>
                     </select>
                     <select>
                         <option>Sent</option>
@@ -49,12 +112,12 @@ const Log: React.FC = () => {
                     </select>
                     <div>
                         <p>Filtering: </p>
-                        <button>ON</button>
+                    <button onClick={() => setFilter(!filter)}>{filter ? "ON" : "OFF"}</button>
                     </div>
                 </div>
                 <div className="log__table-data-container">
-                    {alarms.length > 0 && <LogTable alarmLogs={alarms} setAlarm={al => setAlarm(al)}/>}
-                    {alarms.length === 0 && (
+                    {filterList.length > 0 && <LogTable alarmLogs={filterList} setAlarm={al => setAlarm(al)}/>}
+                    {filterList.length === 0&& (
                         <div className="log__table-container-no-alarms">
                             {!loading && <h1>No alarm logs could be found</h1>}
                             {loading && <h1>Loading all logs...</h1>}
