14e19786703adaef64492acd1b5c9204f2aecd95
Frederik Jensen <frederikhj0@gmail.com>
2021-01-07 20:18:21 +0100

    Uses published functions URL instead of localhost

--- alert-system-webapp/src/data/AlarmLogService.ts
+++ alert-system-webapp/src/data/AlarmLogService.ts
@@ -1,36 +1,37 @@
-import {AlarmLog} from './models/AlarmLog';
-import {AlarmLog as AlarmLogModel, AlarmLogType} from '../models/AlarmLog';
+import { AlarmLog } from './models/AlarmLog';
+import { AlarmLog as AlarmLogModel, AlarmLogType } from '../models/AlarmLog';
 import * as AuthService from './AuthService';
 
 export const GetAlarms = async (): Promise<AlarmLogModel[]> => {
+  const alarmLogs: AlarmLog[] = await fetch(
+    'https://alarm-system-functions.azurewebsites.net/api/alarmlog',
+    { headers: AuthService.Headers() }
+  ).then((response) => response.json());
 
-    const alarmLogs: AlarmLog[] = await fetch('http://localhost:7071/api/alarmlog', {headers: AuthService.Headers()}).then(response => response.json());
-
-    return alarmLogs.map(alarmLog => ({
-        alarm: alarmLog.alarm,
-        machine: alarmLog.machine,
-        date: new Date(alarmLog.date),
-        lastStatus: AlarmLogType.FIXED //Mocked attribute
-    }));
+  return alarmLogs.map((alarmLog) => ({
+    alarm: alarmLog.alarm,
+    machine: alarmLog.machine,
+    date: new Date(alarmLog.date),
+    lastStatus: AlarmLogType.FIXED, //Mocked attribute
+  }));
 };
 
-export const GetFormattedDate = (dateToFormat: Date) : string => {
+export const GetFormattedDate = (dateToFormat: Date): string => {
+  const date = ConvertOneDecimalToTwo(dateToFormat.getDate());
+  const month = ConvertOneDecimalToTwo(dateToFormat.getMonth() + 1); //First month is index zero. So plus one for the correct month
+  const year = dateToFormat.getFullYear();
+  const hours = ConvertOneDecimalToTwo(dateToFormat.getHours());
+  const minutes = ConvertOneDecimalToTwo(dateToFormat.getMinutes());
 
-    const date = ConvertOneDecimalToTwo(dateToFormat.getDate());
-    const month = ConvertOneDecimalToTwo(dateToFormat.getMonth() + 1); //First month is index zero. So plus one for the correct month
-    const year = dateToFormat.getFullYear();
-    const hours = ConvertOneDecimalToTwo(dateToFormat.getHours());
-    const minutes = ConvertOneDecimalToTwo(dateToFormat.getMinutes());
+  const formattedDate = `${date}/${month}/${year} ${hours}:${minutes}`;
 
-    const formattedDate = `${date}/${month}/${year} ${hours}:${minutes}`
-
-    return formattedDate;
+  return formattedDate;
 };
 
-const ConvertOneDecimalToTwo = (decimal: number) : string => {
-    if (decimal < 10) {
-        return `0${decimal}`
-    }
+const ConvertOneDecimalToTwo = (decimal: number): string => {
+  if (decimal < 10) {
+    return `0${decimal}`;
+  }
 
-    return `${decimal}`
-}
+  return `${decimal}`;
+};
--- alert-system-webapp/src/data/AuthService.ts
+++ alert-system-webapp/src/data/AuthService.ts
@@ -1,71 +1,70 @@
-import {User} from "./models/User";
-import {User as UserModel } from "../models/User";
+import { User } from './models/User';
+import { User as UserModel } from '../models/User';
 
+export const LoginWithNameAndEmail = async (
+  name: string,
+  password: string
+): Promise<boolean> => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ password: password, name: name }),
+  };
 
-
-export const LoginWithNameAndEmail = async (name: string, password: string): Promise<boolean>  => {
-
-
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ password: password, name: name })
-    };
-
-    const response =  await fetch('http://localhost:7071/api/token', requestOptions );
-    if(response.ok){
-        const data = await response.json();
-        SetToken(data.token);
-        SetNameOfUser(data.name);
-        SetEmail(data.email);
-        return true;
-    }
-    else {
-        return false;
-    }
+  const response = await fetch(
+    'https://alarm-system-functions.azurewebsites.net/api/token',
+    requestOptions
+  );
+  if (response.ok) {
+    const data = await response.json();
+    SetToken(data.token);
+    SetNameOfUser(data.name);
+    SetEmail(data.email);
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export const Headers = () => {
-    let header = {
-        'Content-Type':'application/json',
-        'Authorization': 'Bearer ' + (sessionStorage.getItem('token') != null ? GetToken() : '')
-    };
-    return header;
+  let header = {
+    'Content-Type': 'application/json',
+    Authorization:
+      'Bearer ' + (sessionStorage.getItem('token') != null ? GetToken() : ''),
+  };
+  return header;
 };
 
-
 export const CheckAuth = (): boolean => {
-    const token = GetToken();
-    if(token != "" && token != undefined){
-        return true;
-    }
-    return false;
+  const token = GetToken();
+  if (token != '' && token != undefined) {
+    return true;
+  }
+  return false;
 };
 
 export const GetToken = (): string | null => {
-    return sessionStorage.getItem('token');
+  return sessionStorage.getItem('token');
 };
 
 export const GetNameOfUser = (): string | null => {
-    return  sessionStorage.getItem('name');
+  return sessionStorage.getItem('name');
 };
 
 export const SetToken = (token: string) => {
-    sessionStorage.setItem('token', token);
+  sessionStorage.setItem('token', token);
 };
 
 export const SetNameOfUser = (name: string) => {
-    sessionStorage.setItem('name', name);
+  sessionStorage.setItem('name', name);
 };
 
 export const SetEmail = (email: string) => {
-    sessionStorage.setItem('email', email);
+  sessionStorage.setItem('email', email);
 };
 
 export const SetLogout = (): boolean => {
-    sessionStorage.removeItem('token');
-    sessionStorage.removeItem('email');
-    return true;
+  sessionStorage.removeItem('token');
+  sessionStorage.removeItem('email');
+  return true;
 };
-
-
